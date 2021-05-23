eea6fac1058e0375fd8ac0e71fbb467d8bce5842
BerkeleyErwin <berwin@ucsd.edu>
2021-05-22 21:56:38 -0700

    Part 1 done starting part 2

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -1,4 +1,27 @@
 /**
  * @jest-environment jsdom
  */
+ import { pushToHistory } from '../scripts/router.js';
+
+ describe('pushToHistory', () => {
+    test('Settings Test', () => {
+        var pushSetting = pushToHistory('settings',1);
+        expect(pushSetting.state).toEqual({page: 'settings'},'','./#settings');
+        expect(pushSetting.length).toBe(2);
+    });    
+
+    test('Entry Test', () => {
+        var pushEntry = pushToHistory('entry',1);
+        expect(pushEntry.state).toEqual({page: 'entry1'}, '', './#entry1');
+        expect(pushEntry.length).toBe(3);
+    });  
+
+    test('Home Test', () => {
+        var pushHome = pushToHistory('home',1);
+        expect(pushHome.state).toEqual({},'','./');
+        expect(pushHome.length).toBe(4);
+    });  
+
+ });
+
 
