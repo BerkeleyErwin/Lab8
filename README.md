379b8d09496eb738dfb7d30f3a726d1b55fb552f
BerkeleyErwin <berwin@ucsd.edu>
2021-05-23 17:09:12 -0700

    completed lab

--- README.md
+++ README.md
@@ -6,10 +6,21 @@
    1
 
 2. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+   
+   You could use unit test to test if it is performing it's core functions properly, but
+   E2E testing may be prefered here if you wish to test edge cases.
 
 3. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters
 
+    Yes, this is something constant that even though you may change around other things in 
+    the code this unit test will remain a constant unless you decide to change the character length.
+
 4. What do you expect to happen if we run our puppeteer tests with the field “headless” set to true?
 
+    We don't get to see the browser UI performs its actions but it will perform the actions.
+
 5. What would your beforeAll callback look like if you wanted to start from the settings page before every test case?
+
+    Check if we're already on the settings page and if not go there by either using the command page.click(); or directly setting the url using page.goto();
+
 
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -29,12 +29,16 @@ describe('Basic user flow for SPA ', () => {
 
   it('Test3: Clicking first <journal-entry>, new URL should contain /#entry1', async () => {
     // implement test3: Clicking on the first journal entry should update the URL to contain “/#entry1”
-
+    var click = await page.click("journal-entry");
+    expect(page.url()).toContain("#entry1");
   });
 
   it('Test4: On first Entry page - checking page header title', async () => {
     // implement test4: Clicking on the first journal entry should update the header text to “Entry 1” 
-
+    const result = await page.evaluate(() => {
+      return document.querySelector('header > h1').innerHTML;
+    });
+    expect(result).toEqual('Entry 1');
   });
 
   it('Test5: On first Entry page - checking <entry-page> contents', async () => {
@@ -50,58 +54,138 @@ describe('Basic user flow for SPA ', () => {
           }
         }
       */
+        const entryData = await page.evaluate(() => {
+          return document.querySelector('entry-page').entry;
+        });
+        expect(entryData.title).toBe('You like jazz?');
+        expect(entryData.date).toBe('4/25/2021');
+        expect(entryData.content).toBe("According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.");
+        expect(entryData.image.src).toBe('https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455');
+        expect(entryData.image.alt).toBe('bee with sunglasses');
 
   }, 10000);
 
+
   it('Test6: On first Entry page - checking <body> element classes', async () => {
     // implement test6: Clicking on the first journal entry should update the class attribute of <body> to ‘single-entry’
-
+    const result = await page.evaluate(() => {
+      return document.body.className;
+    });
+    expect(result).toEqual('single-entry');
   });
 
   it('Test7: Clicking the settings icon, new URL should contain #settings', async () => {
     // implement test7: Clicking on the settings icon should update the URL to contain “/#settings”
-
+    await page.click("img");
+    expect(page.url()).toContain('#settings');
   });
 
   it('Test8: On Settings page - checking page header title', async () => {
     // implement test8: Clicking on the settings icon should update the header to be “Settings”
-
+    const result = await page.evaluate(() => {
+      return document.querySelector('header > h1').innerHTML;
+    });
+    expect(result).toEqual('Settings');
   });
 
   it('Test9: On Settings page - checking <body> element classes', async () => {
     // implement test9: Clicking on the settings icon should update the class attribute of <body> to ‘settings’
-
+    const result = await page.evaluate(() => {
+      return document.body.className;
+    });
+    expect(result).toEqual('settings');
   });
 
   it('Test10: Clicking the back button, new URL should be /#entry1', async() => {
     // implement test10: Clicking on the back button should update the URL to contain ‘/#entry1’
-
+    await page.goBack();
+    expect(page.url()).toContain('#entry1');
   });
 
   // define and implement test11: Clicking the back button once should bring the user back to the home page
-
+  it('test11: Clicking the back button once should bring the user back to the home page', async() => {
+    await page.goBack();
+    expect(page.url()).toBe("http://127.0.0.1:5500/");
+  });
 
   // define and implement test12: When the user if on the homepage, the header title should be “Journal Entries”
-
+  it('test12: When the user if on the homepage, the header title should be “Journal Entries”', async() => {
+    const result = await page.evaluate(() => {
+      return document.querySelector('header > h1').innerHTML;
+    });
+    expect(result).toEqual('Journal Entries');
+  });
 
   // define and implement test13: On the home page the <body> element should not have any class attribute 
-
+  it('test13: On the home page the <body> element should not have any class attribute', async() => {
+    const result = await page.evaluate(() => {
+      return document.body.className;
+    });
+    expect(result).toEqual('');
+  });
 
   // define and implement test14: Verify the url is correct when clicking on the second entry
-
+  it('test14: Verify the url is correct when clicking on the second entry', async() => {
+    const entries = await page.$$('journal-entry');
+    await entries[1].click();
+    await page.waitForNavigation();
+    expect(page.url()).toContain('#entry2');
+  });
 
   // define and implement test15: Verify the title is current when clicking on the second entry
-
+  it('test15: Verify the title is current when clicking on the second entry', async() => {
+    const result = await page.evaluate(() => {
+      return document.querySelector('header > h1').innerHTML;
+    });
+    expect(result).toEqual('Entry 2');
+  });
 
   // define and implement test16: Verify the entry page contents is correct when clicking on the second entry
-
+  it('test16: Verify the entry page contents is correct when clicking on the second entry', async() => {
+    const result = await page.evaluate(() => {
+      return document.querySelector('entry-page').entry;
+    });
+    expect(result.title).toBe('Run, Forrest! Run!');
+    expect(result.date).toBe('4/26/2021');
+    expect(result.content).toBe("Mama always said life was like a box of chocolates. You never know what you're gonna get.");
+    expect(result.image.src).toBe('https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg');
+    expect(result.image.alt).toBe('forrest running');
+  }, 10000);
 
   // create your own test 17
+  it('test17: Verify the url is correct when clicking on the third entry', async() => {
+    await page.goto("http://127.0.0.1:5500/");
+    const entries = await page.$$('journal-entry');
+    await entries[2].click();
+    await page.waitForNavigation();
+    expect(page.url()).toContain('#entry3');
+  }, 10000);
 
   // create your own test 18
+  it('test18: Verify the title is current when clicking on the third entry', async() => {
+    const result = await page.evaluate(() => {
+      return document.querySelector('header > h1').innerHTML;
+    });
+    expect(result).toEqual('Entry 3');
+  });
 
   // create your own test 19
-
-  // create your own test 20
+  it('test19: Verify the entry page contents is correct when clicking on the third entry', async() => {
+    const result = await page.evaluate(() => {
+      return document.querySelector('entry-page').entry;
+    });
+    expect(result.title).toBe('Ogres are like onions');
+    expect(result.date).toBe('4/27/2021');
+    expect(result.content).toBe("Onions have layers. Ogres have layers. Onions have layers. You get it? We both have layers.");
+    expect(result.image.src).toBe('https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.syracuse.com/home/syr-media/width2048/img/entertainment_impact/photo/shrek-donkeyjpg-daa31aa2b5bedfaa.jpg');
+    expect(result.image.alt).toBe('shrek and donkey looking confused');
+  }, 10000);
   
+  // create your own test 20
+  it('test20: Clicking settings should work from entry', async () => {
+    // implement test7: Clicking on the settings icon should update the URL to contain “/#settings”
+    await page.click("img");
+    expect(page.url()).toContain('#settings');
+  });
+
 });
